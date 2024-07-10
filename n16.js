cd9e4912bf51c13cca6407ded948c27b874886cc
SongRang1218 <hhth3546@gmail.com>
2024-07-10 16:59:23 +0900

    17:00

--- n16.js
+++ n16.js
@@ -4,13 +4,19 @@ const fs = require('fs');
 const logger = require('morgan');
 const app = express();
 const port = 3000;
-const _path = path.join(__dirname, '/HTML');
+const _path = path.join(__dirname, '/');
 app.use(logger('tiny'));
 app.use('/', express.static(_path));
+
 app.get('/data', (req, res) => {
-    const title = req.query.content;
+    const title = req.query.title;
     const content = req.query.content;
     console.log(title, content);
+    fs.writeFile(`${_path}${title}.txt`, content, (e) => {
+        if (e) console.log(e); //에러시  에러 내용 출력
+        console.log(`${title}.txt 파일작성이 완료되었습니다.`);
+    });
+    res.redirect('/');
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
