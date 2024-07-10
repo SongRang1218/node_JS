77e632a878230e8947cd1de42e262a9ac2155e4f
SongRang1218 <hhth3546@gmail.com>
2024-07-10 11:49:27 +0900

    n14수정중

---
+++ n14.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const logger = require('morgan');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('안녕하세요! express로 만든 서버입니다.');
+});
+
+app.get('/list', (req, res) => {
+    fs.readdir(__dirname, 'utf-8', (err, data) => {
+        res.send(data);
+    });
+});
+
+app.listen(port, () => {
+    console.log(port + '에서 서버가 시작되었습니다.');
+});
