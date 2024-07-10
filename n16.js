3c22a8f99b4509d105ce4d3cd375a87866332475
SongRang1218 <hhth3546@gmail.com>
2024-07-10 15:56:13 +0900

    변경

--- n16.js
+++ n16.js
@@ -4,10 +4,14 @@ const fs = require('fs');
 const logger = require('morgan');
 const app = express();
 const port = 3000;
-const _path = path.join(__dirname, '/');
+const _path = path.join(__dirname, '/HTML');
 app.use(logger('tiny'));
-app.use();
-
+app.use('/', express.static(_path));
+app.get('/data', (req, res) => {
+    const title = req.query.content;
+    const content = req.query.content;
+    console.log(title, content);
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
