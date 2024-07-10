060eb1d235963ab1f7dc4b00ef8380d59ce7e267
SongRang1218 <hhth3546@gmail.com>
2024-07-10 15:16:22 +0900

    그만떠

--- n13개선.js
+++ n13개선.js
@@ -15,7 +15,6 @@ app.get('/', (req, res) => {
 
 app.get('/story', (req, res) => {
     const arr = ['my life is pretty', 'Egg is Life', 'Cute & I do not have cat', 'Avengers are Dead'];
-
     let list = `<h1>링크를 선택하세요</h1><h2><ul>`;
     const title = ['Pretty', 'Egg', 'Dog', 'Avengers'];
     title.forEach((v, i) => (list += `<li><a href="/story?id=${i}">${v}</a></li>`));
