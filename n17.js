8136c38a981607bf5739bd7a90e8bd367f83e966
SongRang1218 <hhth3546@gmail.com>
2024-07-10 17:28:03 +0900

    17:27

--- n17.js
+++ n17.js
@@ -7,6 +7,31 @@ const port = 3000;
 const _path = path.join(__dirname, '/');
 app.use(logger('tiny'));
 app.use('/', express.static(_path));
+app.get('/', (req, res) => {
+    res.send('안녕하세요! express로 만든 서버입니다.<h2><a href="/list">리스트목록</a></h2>');
+});
+
+// app.get('/data2', (req, res) => {
+//     const title = req.query.title;
+//     const content = req.query.content;
+//     console.log(title, content);
+//     fs.writeFile(`${_path}${title}.txt`, content, (e) => {
+//         if (e) console.log(e);
+//         console.log(`${title}.txt 파일작성이 완료되었습니다.`);
+//     });
+// });
+
+app.get('/list', (req, res) => {
+    fs.readdir(__dirname, 'utf-8', (err, data) => {
+        let list = `<h1>링크를 선택하세요</h1><h2><ul>`;
+        data.forEach(
+            (v) =>
+                (list += `<li><a href='${v}'style= 'text-decoration-line :none;'>${v}</a><a href='${v}'style= 'text-decoration-line :none;', download> - [DownLoad]</a></li>`)
+        );
+        list += `</ul></h2>`;
+        res.send(list);
+    });
+});
 
 app.listen(port, () => {
     console.log('서버를 시작합니다.');
