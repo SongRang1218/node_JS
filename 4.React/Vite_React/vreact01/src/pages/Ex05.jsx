7b8e4b1cde61fc846c701d32301e888270818da0
SongRang1218 <hhth3546@gmail.com>
2024-08-13 15:59:52 +0900

    2024-08-13

--- 4.React/Vite_React/vreact01/src/pages/Ex05.jsx
+++ 4.React/Vite_React/vreact01/src/pages/Ex05.jsx
@@ -1,19 +1,37 @@
 import { useState } from 'react';
 const Arrays = () => {
-    const [arr, setArr] = useState('');
-    let inData = '';
-    const handleInput = (e) => (inData = e.target.value);
-    const handleAdd = (e) => setArr([...arr, inData]);
-    const handleDel = (e) => {};
+    const [arr, setArr] = useState([]);
+    const [indata, setIndata] = useState('');
+    const handleInput = (e) => setIndata(e.target.value);
+    const handleAdd = (e) => {
+        setArr([...arr, indata]);
+        setIndata('');
+    };
+    const handleDel = (e) => {
+        setArr(arr.slice(0, -1));
+    };
+    const handleADel = (e) => {
+        setArr([arr.pop]);
+    };
 
     return (
         <>
             <h1>5. 어레이 실시간 추가</h1>
+            {/* <div>{arr.length > 0 ? arr : '배열이 비었습니다.'}</div> */}
             <label htmlFor="inin">배열요소 입력 : </label>
-            <input type="text" id="inin" onChange={handleInput} />
-            <button onClick={handleAdd}>추가</button>
-            <button onClick={handleDel}>제거</button>
-            <h3>{arr}</h3>
+            <input type="text" id="inin" onChange={handleInput} value={indata} />
+            <button onClick={handleAdd} disabled={!indata}>
+                추가
+            </button>
+            <button onClick={handleDel} disabled={!arr.length > 0}>
+                제거
+            </button>
+            <button onClick={handleADel}>전부제거</button>
+            <h3>
+                {arr.map((v) => (
+                    <span key={v}> {v} </span>
+                ))}
+            </h3>
         </>
     );
 };
